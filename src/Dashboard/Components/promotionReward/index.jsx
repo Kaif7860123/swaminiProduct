3eae6a328f68ab3da2201224886641aa9b4bdd35
kaif7860123 <kaif47320@gmail.com>
2025-05-22 21:27:45 +0530

    first commit

--- src/Dashboard/Components/promotionReward/index.jsx
+++ src/Dashboard/Components/promotionReward/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AdminTitltSection from '../AdminTitleSection'
 import Tables from '../Tables'
-import { userpromotionReward } from '../../data/UserTable/PromotionReward'
+import { userpromotionReward } from '../../data/UserTable/promotionReward'
  import styles from './index.module.css'
 const PromotionReward = () => {
   return (
